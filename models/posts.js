5cba3e647c05737851f4a86a3954ba95c81db726
Chester Zhang <Chester Zhang>
2017-05-02 13:58:59 +0800

    post & edit & remove

--- models/posts.js
+++ models/posts.js
@@ -9,7 +9,7 @@ Post.plugin('contentToHtml', {
 			return post
 		})
 	},
-	afterFindOne: function(posts) {
+	afterFindOne: function(post) {
 		if(post) {
 			post.content = marked(post.content)
 		}
@@ -22,30 +22,45 @@ module.exports = {
 	},
 	getPostById: function getPostById(postId) {
 		return Post
-			.findOne({_id: postId})
-			.populate({path: 'author', model: 'User'})
-			.addCreatedAt()
-			.contentToHtml()
-			.exec()
+		.findOne({_id: postId})
+		.populate({path: 'author', model: 'User'})
+		.addCreatedAt()
+		.contentToHtml()
+		.exec()
 	},
 	//按时间降序获取文章
 	getPosts: function getPosts(author) {
-		var query = {},
+		var query = {}
 		if(author) {
 			query.author = author
 		}
 		return Post
-			.find(query)
-			.populate({path: 'author', model: 'User'})
-			.addCreatedAt()
-			.contentToHtml()
-			.exec()
+		.find(query)
+		.populate({path: 'author', model: 'User'})
+		.addCreatedAt()
+		.contentToHtml()
+		.exec()
 	},
 	//增加点击量
 	incPv: function incPv(postId) {
 		return Post
-			.update({_id: postId}, {$inc: {pv : 1}})
-			.exec()
+		.update({_id: postId}, {$inc: {pv : 1}})
+		.exec()
+	},
+	getRawPostById: function getRawPostById(postId) {
+		return Post
+		.findOne({ _id: postId })
+		.populate({ path: 'author', model: 'User' })
+		.exec()
+	},
+	// 通过用户 id 和文章 id 更新一篇文章
+	updatePostById: function updatePostById(postId, author, data) {
+		return Post.update({ author: author, _id: postId }, { $set: data }).exec()
+	},
+
+	// 通过用户 id 和文章 id 删除一篇文章
+	delPostById: function delPostById(postId, author) {
+		return Post.remove({ author: author, _id: postId }).exec()
 	}
 
 }
--- routes/posts.js
+++ routes/posts.js
@@ -8,8 +8,16 @@ var checkLogin = require('../middlewares/check').checkLogin
 // GET /posts 所有用户或者特定用户的文章页
 //   eg: GET /posts?author=xxx
 router.get('/', function(req, res, next) {
-	res.render('posts')
-})
+	var author = req.query.author
+
+	PostModel.getPosts(author)
+	.then(function (posts) {
+		res.render('posts', {
+			posts: posts
+		})
+	})
+	.catch(next)
+});
 
 // POST /posts 发表一篇文章
 router.post('/', checkLogin, function(req, res, next) {
@@ -37,12 +45,12 @@ router.post('/', checkLogin, function(req, res, next) {
 	}
 
 	PostModel.publish(post)
-		.then(function(result) {
-			post = result.ops[0]
-			req.flash('sucess', '发表成功')
-			res.redirect(`/posts/${post._id}`)
-		})
-		.catch(next)
+	.then(function(result) {
+		post = result.ops[0]
+		req.flash('sucess', '发表成功')
+		res.redirect(`/posts/${post._id}`)
+	})
+	.catch(next)
 })
 
 // GET /posts/publish 发表文章页
@@ -52,22 +60,72 @@ router.get('/publish', checkLogin, function(req, res, next) {
 
 // GET /posts/:postId 单独一篇的文章页
 router.get('/:postId', function(req, res, next) {
-	res.send(req.flash())
+	var postId = req.params.postId
+
+	Promise.all([
+		PostModel.getPostById(postId),// 获取文章信息
+		PostModel.incPv(postId)// pv 加 1
+		])
+	.then(function (result) {
+		var post = result[0]
+		if (!post) {
+			throw new Error('该文章不存在')
+		}
+		res.render('post', {
+			post: post
+		})
+	})
+	.catch(next)
 })
 
 // GET /posts/:postId/edit 更新文章页
 router.get('/:postId/edit', checkLogin, function(req, res, next) {
-	res.send(req.flash())
+	var postId = req.params.postId
+	var author = req.session.user._id
+
+	PostModel.getRawPostById(postId)
+	.then(function (post) {
+		if (!post) {
+			throw new Error('该文章不存在')
+		}
+		if (author.toString() !== post.author._id.toString()) {
+			throw new Error('权限不足')
+		}
+		res.render('edit', {
+			post: post
+		})
+	})
+	.catch(next)
 })
 
 // POST /posts/:postId/edit 更新一篇文章
 router.post('/:postId/edit', checkLogin, function(req, res, next) {
-	res.send(req.flash())
+	var postId = req.params.postId
+	var author = req.session.user._id
+	var title = req.fields.title
+	var content = req.fields.content
+
+	PostModel.updatePostById(postId, author, { title: title, content: content })
+	.then(function () {
+		req.flash('success', '编辑文章成功')
+		// 编辑成功后跳转到上一页
+		res.redirect(`/posts/${postId}`)
+	})
+	.catch(next)
 })
 
 // GET /posts/:postId/remove 删除一篇文章
 router.get('/:postId/remove', checkLogin, function(req, res, next) {
-	res.send(req.flash())
+	var postId = req.params.postId
+	var author = req.session.user._id
+
+	PostModel.delPostById(postId, author)
+	.then(function () {
+		req.flash('success', '删除文章成功')
+		// 删除成功后跳转到主页
+		res.redirect('/posts')
+	})
+	.catch(next)
 })
 
 // POST /posts/:postId/comment 创建一条留言
